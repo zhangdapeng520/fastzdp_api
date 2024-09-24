35546c2dd13fcb31e271284c9c5bfa6950e5cdf7
张大鹏 <1156956636@qq.com>
2024-09-24 19:58:54 +0800

    新增: 查询接口支持等值查询

--- fastzdp_api/crud_router.py
+++ fastzdp_api/crud_router.py
@@ -21,26 +21,45 @@ def get_crud_router(
     router = APIRouter(tags=[f"{name} 管理"])
 
     @router.post(f"/{path}", summary=f"新增 {name}")
-    async def add_classes(req_data: modelSchema):
+    async def add_model_router(req_data: modelSchema):
         fasm.add(engine, model(**req_data.model_dump()))
         return None
 
     @router.put(f"/{path}" + "/{id}", summary=f"更新 {name}")
-    async def update_classes(id: int, req_data: modelSchema):
-        classes = fasm.get(engine, model, id)
-        if not classes:
+    async def update_model_router(id: int, req_data: modelSchema):
+        model_router = fasm.get(engine, model, id)
+        if not model_router:
             return JSONResponse(status_code=404, content={"error": "数据不存在"})
 
-        fasm.update(engine, classes, req_data.model_dump())
+        fasm.update(engine, model_router, req_data.model_dump())
         return None
 
     @router.delete(f"/{path}" + "/{id}", summary=f"删除 {name}")
-    async def delete_classes(id: int):
+    async def delete_model_router(id: int):
         fasm.delete_id(engine, model, id)
         return None
 
     @router.get(f"/{path}", summary=f"查询 {name}")
-    async def get_classes(page: int = 1, size: int = 8):
-        return fasm.get_page(engine, model, page, size)
+    async def get_model_router(
+            page: int = 1,
+            size: int = 8,
+            key1: str = None,
+            value1: str = None,
+            key2: str = None,
+            value2: str = None,
+            key3: str = None,
+            value3: str = None,
+    ):
+        # 等值查询参数
+        query_dict = {}
+        if key1 and value1:
+            query_dict[key1] = value1
+        if key2 and value2:
+            query_dict[key2] = value2
+        if key3 and value3:
+            query_dict[key3] = value3
+
+        # 执行查询
+        return fasm.get_page(engine, model, page, size, **query_dict)
 
     return router
